69d9a3aa35b64cfbb202b282640692e51dac134b
nicolai jensen <nbje63269@edu.ucl.dk>
2024-03-15 09:05:08 +0100

    Add project files.

---
+++ Projekt 2 - Gensalg/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projekt_2___Gensalg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
