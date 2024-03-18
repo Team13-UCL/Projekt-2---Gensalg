3d2aa57ca3ebd3747a2ae16c7892bd86bd4deb18
Nicolai.Tofteby <nicolaitofteby@gmail.com>
2024-03-18 09:20:57 +0100

    Fucked

--- Projekt 2 - Gensalg/Program.cs
+++ Projekt 2 - Gensalg/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hej gruppe 13 :D");
-            Console.WriteLine("Hej gruppe :D");
+            Console.WriteLine("Jeg har fucked det op");
             Console.ReadLine();
         }
     }
